47ef361246eb2aaec4c9c2748b85cd37fcd35318
thomas.friedel <thomas.friedel@gmail.com>
2018-01-10 19:08:19 +0100

    - added spearman's rank correlation coefficient - allow to calculate mean / std for arrays consisting of multiple rows

--- utils/score_utils.py
+++ utils/score_utils.py
@@ -1,14 +1,38 @@
 import numpy as np
+from scipy.stats import spearmanr
 
-# calculate mean score for AVA dataset
 def mean_score(scores):
-    si = np.arange(1, 11, 1)
-    mean = np.sum(scores * si)
+    """ calculate mean score for AVA dataset
+    :param scores:
+    :return: row wise mean score if scores contains multiple rows, else
+             a single mean score
+    """
+    si = np.arange(1, 11, 1).reshape(1,10)
+    mean = np.sum(scores * si, axis=1)
+    if mean.shape==(1,):
+        mean = mean[0]
     return mean
 
-# calculate standard deviation of scores for AVA dataset
+
 def std_score(scores):
-    si = np.arange(1, 11, 1)
-    mean = mean_score(scores)
-    std = np.sqrt(np.sum(((si - mean) ** 2) * scores))
+    """ calculate standard deviation of scores for AVA dataset
+    :param scores:
+    :return: row wise standard deviations if scores contains multiple rows,
+             else a single standard deviation
+    """
+    si = np.arange(1, 11, 1).reshape(1,10)
+    mean = mean_score(scores).reshape(-1,1)
+    std = np.sqrt(np.sum(((si - mean) ** 2) * scores, axis=1))
+    if std.shape==(1,):
+        std = std[0]
     return std
+
+
+def srcc(y_test, y_pred):
+    """ calculate spearman's rank correlation coefficient
+    :param y_test: the human ratings (width 10)
+    :param y_pred: the predicted ratings (width 10)
+    :return:
+    """
+    rho, pValue = spearmanr(mean_score(y_test), mean_score(y_pred))
+    return rho
