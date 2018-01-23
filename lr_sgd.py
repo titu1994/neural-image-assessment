45d819106386a6e1d620392076919bdc0567c87a
thomas.friedel <thomas.friedel@gmail.com>
2018-01-23 20:13:52 +0100

    added SGD with the ability to change learning rate per layer. unused atm. added example pics.

---
+++ lr_sgd.py
@@ -0,0 +1,72 @@
+
+from keras.legacy import interfaces
+import keras.backend as K
+from keras.optimizers import Optimizer
+
+class LR_SGD(Optimizer):
+    """Stochastic gradient descent optimizer.
+
+    Includes support for momentum,
+    learning rate decay, and Nesterov momentum.
+
+    # Arguments
+        lr: float >= 0. Learning rate.
+        momentum: float >= 0. Parameter updates momentum.
+        decay: float >= 0. Learning rate decay over each update.
+        nesterov: boolean. Whether to apply Nesterov momentum.
+    """
+
+    def __init__(self, lr=0.01, momentum=0., decay=0.,
+                 nesterov=False,multipliers=None,**kwargs):
+        super(LR_SGD, self).__init__(**kwargs)
+        with K.name_scope(self.__class__.__name__):
+            self.iterations = K.variable(0, dtype='int64', name='iterations')
+            self.lr = K.variable(lr, name='lr')
+            self.momentum = K.variable(momentum, name='momentum')
+            self.decay = K.variable(decay, name='decay')
+        self.initial_decay = decay
+        self.nesterov = nesterov
+        self.lr_multipliers = multipliers
+
+    @interfaces.legacy_get_updates_support
+    def get_updates(self, loss, params):
+        grads = self.get_gradients(loss, params)
+        self.updates = [K.update_add(self.iterations, 1)]
+
+        lr = self.lr
+        if self.initial_decay > 0:
+            lr *= (1. / (1. + self.decay * K.cast(self.iterations,
+                                                  K.dtype(self.decay))))
+        # momentum
+        shapes = [K.int_shape(p) for p in params]
+        moments = [K.zeros(shape) for shape in shapes]
+        self.weights = [self.iterations] + moments
+        for p, g, m in zip(params, grads, moments):
+
+            if p.name in self.lr_multipliers:
+                new_lr = lr * self.lr_multipliers[p.name]
+            else:
+                new_lr = lr
+
+            v = self.momentum * m - new_lr * g  # velocity
+            self.updates.append(K.update(m, v))
+
+            if self.nesterov:
+                new_p = p + self.momentum * v - new_lr * g
+            else:
+                new_p = p + v
+
+            # Apply constraints.
+            if getattr(p, 'constraint', None) is not None:
+                new_p = p.constraint(new_p)
+
+            self.updates.append(K.update(p, new_p))
+        return self.updates
+
+    def get_config(self):
+        config = {'lr': float(K.get_value(self.lr)),
+                  'momentum': float(K.get_value(self.momentum)),
+                  'decay': float(K.get_value(self.decay)),
+                  'nesterov': self.nesterov}
+        base_config = super(LR_SGD, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
